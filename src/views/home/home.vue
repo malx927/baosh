0cf9f023f1ae25c36104f9d1bfdaee9ed0473237
mlx927 <5971158@qq.com>
2020-04-20 08:33:04 +0800

    update

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,28 +1,38 @@
 <template>
-    <div>
+  <!-- <div>
         <top-search></top-search>
         <front-slide></front-slide>
         <font-marquee></font-marquee>
-    </div>
-
+  </div>-->
+  <div style="height:100%;">
+    <view-box ref="viewBox">
+      <x-header slot="header" style="width:100%;position:absolute;left:0;top:0;z-index:100;"></x-header>
+      <router-view></router-view>
+      <tabbar slot="bottom"></tabbar>
+    </view-box>
+  </div>
 </template>
 
 <script>
-
-import TopSearch from '@/views/TopSearch'
-import FrontSlide from '@/components/FrontSlide'
-import FontMarquee from '@/components/FontMarquee'
-
+import TopSearch from "@/views/TopSearch";
+import FrontSlide from "@/components/FrontSlide";
+import FontMarquee from "@/components/FontMarquee";
+import {ViewBox} from "vux"
 export default {
-   name: "Home",
-   components:{
-       TopSearch,
-       FrontSlide,
-       FontMarquee,
-   }
-}
+  name: "Home",
+  components: {
+    TopSearch,
+    FrontSlide,
+    FontMarquee,
+    ViewBox,
+  }
+};
 </script>
 
 <style>
-
+html, body {
+  height: 100%;
+  width: 100%;
+  overflow-x: hidden;
+}
 </style>
